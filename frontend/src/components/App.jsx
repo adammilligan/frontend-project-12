6dc9cddc60f06ec2f88799ccc83009153893f029
Вадим Валыгин <vadim12388@mail.ru>
2023-05-02 18:16:13 +0300

    login form

---
+++ frontend/src/components/App.jsx
@@ -0,0 +1,15 @@
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import NotFoundPage from './NotFoundPage';
+import LoginPage from './LoginPage';
+
+const App = () => (
+  <BrowserRouter>
+    <Routes>
+      <Route path="/" element={<LoginPage />} />
+      <Route path="login" element={<LoginPage />} />
+      <Route path="*" element={<NotFoundPage />} />
+    </Routes>
+  </BrowserRouter>
+);
+
+export default App;
---
+++ frontend/src/components/NotFoundPage.jsx
@@ -0,0 +1,10 @@
+const NotFoundPage = () => (
+  <>
+    <h3>Адрес не существует</h3>
+    <div>
+      404
+    </div>
+  </>
+);
+
+export default NotFoundPage;
