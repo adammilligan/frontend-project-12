4b522608e75ec3504c8feb0504b6914177ea0cb7
Вадим Валыгин <vadim12388@mail.ru>
2023-06-05 13:36:42 +0300

    add signUp page

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -1,7 +1,7 @@
 import ReactDOM from 'react-dom/client';
 import init from './init.jsx';
 
-import 'bootstrap/dist/css/bootstrap.min.css';
+import './styles/application.scss';
 
 const app = async () => {
   const root = ReactDOM.createRoot(document.getElementById('root'));
