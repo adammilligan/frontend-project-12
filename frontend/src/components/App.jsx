74badac6665fa0f14557a9bef0283bb94533a918
Вадим Валыгин <vadim12388@mail.ru>
2023-06-09 14:54:42 +0300

    add rollbar

--- frontend/src/components/App.jsx
+++ frontend/src/components/App.jsx
@@ -14,6 +14,7 @@ import { useTranslation } from 'react-i18next';
 import { Navbar, Container, Button } from 'react-bootstrap';
 import { BsGlobe } from 'react-icons/bs';
 import { ToastContainer, toast } from 'react-toastify';
+import { Provider, ErrorBoundary } from '@rollbar/react';
 
 import ChatPage from './ChatPage/ChatPage.jsx';
 import LoginPage from './LoginPage/LoginPage.jsx';
@@ -21,6 +22,15 @@ import NotFoundPage from './NotFoundPage/NotFoundPage.jsx';
 import SignUpPage from './SignUpPage/SignUpPage.jsx';
 import { useAuth } from '../hooks';
 import routes from '../routes/routes.js';
+
+const rollbarConfig = {
+  accessToken: process.env.REACT_APP_ROLLBAR_ACCESS_TOKEN,
+  payload: {
+    environment: 'production',
+  },
+  captureUncaught: true,
+  captureUnhandledRejections: true,
+};
 
 const PrivateRoute = ({ children }) => {
   const auth = useAuth();
@@ -54,44 +64,48 @@ const App = () => {
   };
 
   return (
-    <div className="d-flex flex-column h-100">
-      <Router>
-        <Navbar bg="white" expand="lg" className="shadow-sm">
-          <Container>
-            <Navbar.Brand as={Link} to={routes.chatPagePath()}>{t('chatLogo')}</Navbar.Brand>
-            <Button onClick={() => handleLangChange(i18n)} variant="group-vertical">
-              <BsGlobe size="35" />
-            </Button>
-            <AuthButton translation={t} />
-          </Container>
-        </Navbar>
-        <Routes>
-          <Route
-            path={routes.chatPagePath()}
-            element={(
-              <PrivateRoute>
-                <ChatPage />
-              </PrivateRoute>
-            )}
-          />
-          <Route path={routes.loginPagePath()} element={<LoginPage />} />
-          <Route path={routes.notFoundPagePath()} element={<NotFoundPage />} />
-          <Route path={routes.signupPagePath()} element={<SignUpPage />} />
-        </Routes>
-        <ToastContainer
-          position="top-right"
-          autoClose={5000}
-          hideProgressBar={false}
-          newestOnTop={false}
-          closeOnClick
-          rt1={false}
-          pauseOnFocusLoss
-          draggable
-          pauseOnHover
-          theme="light"
-        />
-      </Router>
-    </div>
+    <Provider config={rollbarConfig}>
+      <ErrorBoundary>
+        <div className="d-flex flex-column h-100">
+          <Router>
+            <Navbar bg="white" expand="lg" className="shadow-sm">
+              <Container>
+                <Navbar.Brand as={Link} to={routes.chatPagePath()}>{t('chatLogo')}</Navbar.Brand>
+                <Button onClick={() => handleLangChange(i18n)} variant="group-vertical">
+                  <BsGlobe size="35" />
+                </Button>
+                <AuthButton translation={t} />
+              </Container>
+            </Navbar>
+            <Routes>
+              <Route
+                path={routes.chatPagePath()}
+                element={(
+                  <PrivateRoute>
+                    <ChatPage />
+                  </PrivateRoute>
+                )}
+              />
+              <Route path={routes.loginPagePath()} element={<LoginPage />} />
+              <Route path={routes.notFoundPagePath()} element={<NotFoundPage />} />
+              <Route path={routes.signupPagePath()} element={<SignUpPage />} />
+            </Routes>
+            <ToastContainer
+              position="top-right"
+              autoClose={5000}
+              hideProgressBar={false}
+              newestOnTop={false}
+              closeOnClick
+              rt1={false}
+              pauseOnFocusLoss
+              draggable
+              pauseOnHover
+              theme="light"
+            />
+          </Router>
+        </div>
+      </ErrorBoundary>
+    </Provider>
   );
 };
 
